e31aa2136db12148e6e59d3cff19e943e632f0aa
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-06-22 22:07:27 +0300

    Created the models

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { DatabaseModule } from './database/database.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
+  imports: [TasksModule, DatabaseModule, UserModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
